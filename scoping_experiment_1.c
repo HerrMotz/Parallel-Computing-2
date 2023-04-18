e04c447a726f8acfb184d92eec7b97dbf7ddab79
Daniel Motz <daniel.motz@mandant3.de>
2023-04-18 13:46:24 +0200

    rename omp_ex

--- scoping_experiment_1.c
+++ scoping_experiment_1.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <omp.h>
-
-static int a = 0;
-
-int main()
-{
-    int b = 0;
+static int a=0;
+int main() {
+    int b=0;
     omp_set_num_threads(10);
-    #pragma omp parallel // default(none) shared(a, b)
+    #pragma omp parallel default(none) shared(a, b)
     {
-        int c = 0;
-        static int d = 0;
+        int c=0;
+        static int d=0;
         a++;
         b++;
         c++;
         d++;
-        printf("Thread, (a, b, c, d) = %d, (%d, %d, %d, %d)\n", omp_get_thread_num(), a, b, c, d);
+        printf("Thread, (a,b,c,d) = %d, (%d,%d,%d,%d)\n",
+               omp_get_thread_num(), a, b, c, d);
     }
 
-    printf("Thread, (a, b) = %d (%d, %d)\n", omp_get_thread_num(), a, b);
+    printf("Thread, (a,b) = %d, (%d,%d)\n",
+           omp_get_thread_num(), a, b);
 
     return 0;
 }
