1c0a723be8b632a52200b0dec9bee14fe49c28e3
Daniel Motz <daniel.motz@mandant3.de>
2023-04-18 13:37:02 +0200

    add scoping_experiment_1.c from Lecture II OpenMP

---
+++ scoping_experiment_1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <omp.h>
+
+static int a = 0;
+
+int main()
+{
+    int b = 0;
+    omp_set_num_threads(10);
+    #pragma omp parallel // default(none) shared(a, b)
+    {
+        int c = 0;
+        static int d = 0;
+        a++;
+        b++;
+        c++;
+        d++;
+        printf("Thread, (a, b, c, d) = %d, (%d, %d, %d, %d)\n", omp_get_thread_num(), a, b, c, d);
+    }
+
+    printf("Thread, (a, b) = %d (%d, %d)\n", omp_get_thread_num(), a, b);
+
+    return 0;
+}
